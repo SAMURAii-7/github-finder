229af9074402ab55a47db23815aae5de9abbccc9
Shubham <shubham.prasad21@gmail.com>
2022-05-24 19:55:35 +0530

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p className="text-3xl" >This is a React app to search GitHub profiles and see their details.</p>
+            <p className="text-lg text-gray-400" >Version: <span className="text-white">1.0.0</span></p>
+            <p className="text-lg text-gray-400" >Made By: <span className="text-white">Shubham Prasad</span></p>
+        </div>
+    )
+}
+
+export default About
