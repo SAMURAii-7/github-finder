d6cfb8b3997d78928fe4addeb101028e981097db
Shubham <shubham.prasad21@gmail.com>
2022-05-30 20:18:52 +0530

    complete search feature and clear button

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,28 +15,36 @@ export const GithubProvider = ({ children }) => {
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
     // Get initial users for testing
-    const fetchUsers = async () => {
+    const searchUsers = async (text) => {
         setLoading()
-        const res = await fetch(`${GITHUB_URL}/users`, {
+
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
 
-        const data = await res.json()
+        const { items } = await res.json()
 
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items
         })
     }
 
     const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers
     }} >
         {children}
     </GithubContext.Provider>
